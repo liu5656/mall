9c54b43a986c4ffd1b768634328c06449b3174d0
x <liujian_haha@163.com>
2021-10-21 10:06:11 +0800

    .

---
+++ mall-admin/src/main/java/com/macro/mall/admin/controller/MinioController.java
@@ -0,0 +1,20 @@
+package com.macro.mall.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @Author lj
+ * @date 2021/10/21 9:29 上午
+ * @desc
+ */
+@Api(tags = "MinioController", description = "MinIO对象存储管理")
+@RestController
+@RequestMapping("/minio")
+public class MinioController {
+
+    //TODO MinIO对象存储管理
+
+}
---
+++ mall-admin/src/main/java/com/macro/mall/admin/controller/OssController.java
@@ -0,0 +1,17 @@
+package com.macro.mall.admin.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @Author lj
+ * @date 2021/10/21 10:01 上午
+ * @desc
+ */
+@Api(tags = "OssController", description = "对象存储管理")
+@RestController
+@RequestMapping("/aliyun/oss")
+public class OssController {
+}
